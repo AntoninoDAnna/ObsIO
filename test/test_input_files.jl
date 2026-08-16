e2800dd54a50c46b98d8d8222315ce85c38ea68a
Antonino D'Anna <anda982008@gmail.com>
2026-08-16 14:21:52 +0200

    added benchmark test

--- test/test_input_files.jl
+++ test/test_input_files.jl
@@ -1,12 +1,16 @@
 using Revise
 using ObsIO
+using BenchmarkTools
 
 ipath = "/home/work/b-physics/data/J307/J307r000.in"
 dpath = "/home/work/b-physics/data/J307/b1/J307r000_cnfg1.mesons.dat"
-corr = ObsIO.read_input_file(ipath,ObsIO.G5,ObsIO.G5)
+corr = @benchmark ObsIO.read_input_file(ipath,ObsIO.G5,ObsIO.G5)
 cdata = ObsIO.read_mesons(dpath,ObsIO.G5,ObsIO.G5)
 corr = ObsIO.corr_obs.(cdata,corr,L=1,real=true)
 
 #Periodic boundary conditions
 ipath = "/home/work/b-physics/data/B450/B450r000.in"
-corr = ObsIO.read_input_file(ipath,ObsIO.G5,ObsIO.G5)
+corr = @benchmark ObsIO.read_input_file(ipath,ObsIO.G5,ObsIO.G5)
+
+
+0==0
